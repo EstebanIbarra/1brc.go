826cf1f1d47cf51d3b92927c88e3dbe23701bd9e
Esteban Ibarra <sectorjeat@gmail.com>
2024-03-18 10:22:30 -0600

    Adds githooks dependencies to setup script

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ chmod +x .githooks/*
 
 # Copy the githooks to the .git/hooks folder
 cp .githooks/* .git/hooks/
+
+# Install the dependencies
+go install golang.org/x/tools/cmd/goimports@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.56.2
